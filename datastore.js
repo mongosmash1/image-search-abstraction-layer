3f6403590f6511f7c54b9584170f4c764f73a4b3
Glitch (ms1-isal-micro) <none>
2018-05-21 23:50:14 +0000

    🔨🐯 Updated with Glitch

--- datastore.js
+++ datastore.js
@@ -3,54 +3,45 @@
 const mongo = require('mongodb').MongoClient;
 // Standard URI format: mongodb://[dbuser:dbpassword@]host:port/dbname, details set in .env
 const MONGODB_URI = 'mongodb://'+process.env.USER+':'+process.env.PASS+'@'+process.env.HOST+':'+process.env.DB_PORT+'/'+process.env.DB;
-const crypto = require('crypto');
 
-//
-// document schema = {
-//   urlCode, (8 digit alphanumeric code)
-//   urlTarget (url of website that code redirects to)
-// }
-//
+/*
+collection = image-searches
+schema = {
+  searchedTerm, (string)
+  searchedTime (javascript time format)
+}
+*/
 
-
-exports.get = function (key, value, callback) {
+// retrieve most recent 10 searches
+exports.get = function (qty, callback) {
   mongo.connect(MONGODB_URI, function(err, client) {
     if (err) throw err;
     const db = client.db(process.env.DB);
-    const url = db.collection(process.env.COLLECTION);
-    var query = {};
-    query[key] = value;
-    url.find(query).project({ _id: 0, urlCode: 1, urlTarget: 1 }).toArray(function (err, docs){
+    const images = db.collection(process.env.COLLECTION);
+    images.find().project({ _id: 0, searchedTerm: 1, searchedTime: 1 }).sort({ searchedTime: -1} ).limit(qty).toArray(function (err, docs){
       if (err) {
         client.close;
         callback(err);
       } else {
-        let results = docs[0];
-        callback(null, results);
+        callback(null, docs);
         client.close;
       }
     });
   });
 }
 
-exports.put = function (urlTarget) {
+// create new document with search term used and current date/time
+exports.put = function (searchedTerm) {
   mongo.connect(MONGODB_URI, function(err, client) {
     if (err) throw err;
     const db = client.db(process.env.DB);
-    const url = db.collection(process.env.COLLECTION);
-    // at some point should add logic to check if 8 digit code is unique in the database
-    let urlCode = generateCode(8);
+    const images = db.collection(process.env.COLLECTION);
     // create document only
-    var doc = { 'urlCode': urlCode, 'urlTarget': urlTarget };
-    url.insert(doc, function(err, doc) {
+    var searchedTime = Date.now()
+    var doc = { searchedTerm: searchedTerm, searchedTime: searchedTime };
+    images.insert(doc, function(err, doc) {
       if (err) throw err;
       client.close();
     });
   });
-}
-
-function generateCode(length) {
-  return crypto.randomBytes(Math.ceil(length / 2))
-        .toString('hex')  // convert to hexadecimal format
-        .slice(0,length); // return required number of characters
 }
